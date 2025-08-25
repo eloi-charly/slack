a5c732f10214448f3eae01c71d4636d53071c9af
eloi-charly <eloi.randriamihaingo@dragonbleu.fr>
2025-08-25 08:33:18 +0200

    back : auth implement

---
+++ backend/src/config/inngest.js
@@ -0,0 +1,46 @@
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "slack-clone" });
+
+const syncUser = inngest.createFunction(
+  { id: "sync-user" },
+  {
+    event: "clerk/user.created",
+  },
+  async ({ event }) => {
+    await connectDB();
+
+    const { id, email_addresses, last_name, first_name, image_url } =
+      event.data;
+    const newUser = {
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}`,
+      image: image_url || "",
+      clerkId: id,
+    };
+
+    await User.create(newUser);
+
+    // TODO: Do more things with the new user
+  }
+);
+
+const deleteUserFromDB = inngest.createFunction(
+  { id: "delete-user-from-db" },
+  {
+    event: "clerk/user.deleted",
+  },
+  async ({ event }) => {
+    await connectDB();
+
+    const { id } = event.data;
+
+    await User.deleteOne({ clerkId: id });
+    // TODO: Do more things with the deleted user
+  }
+);
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [syncUser, deleteUserFromDB];
